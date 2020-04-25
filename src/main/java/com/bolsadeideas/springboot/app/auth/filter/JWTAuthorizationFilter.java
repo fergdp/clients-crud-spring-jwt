43cb0b1f144fc9df8e6dcc4c7b4e5f5681027b8c
Fernando Gomez de Paz <fernando.gomezdepaz@claro.com.ar>
2020-04-25 14:45:13 -0300

    266 - Realizando autenticacion con el token jwt enviado por el cliente

--- src/main/java/com/bolsadeideas/springboot/app/auth/filter/JWTAuthorizationFilter.java
+++ src/main/java/com/bolsadeideas/springboot/app/auth/filter/JWTAuthorizationFilter.java
@@ -1,9 +1,14 @@
 package com.bolsadeideas.springboot.app.auth.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -11,6 +16,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
 
 public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
     public JWTAuthorizationFilter(AuthenticationManager authenticationManager) {
@@ -37,10 +44,19 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
             validoToken = false;
         }
 
-        if (validoToken) {
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = null;
 
+        if (validoToken) {
+            String userName = token.getSubject();
+            Object roles = token.get("authorities");
+
+            Collection<? extends GrantedAuthority> authorities = Arrays.asList(new ObjectMapper().readValue(roles.toString(), SimpleGrantedAuthority[].class));
+
+            usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userName, null, authorities);
         }
 
+        SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+        chain.doFilter(request,response);
     }
 
     private boolean requiresAuthentication(String header) {
